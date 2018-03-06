82dd531df16bdd06b61eff41ec26b5d39bdab106
Maria Betances <mbetances1002@gmail.com>
2018-03-05 23:15:06 -0500

    HackerRank Day0 Weighted Mean

--- weighted_mean.py
+++ weighted_mean.py
@@ -12,6 +12,16 @@ def calculate_weighted_mean(list_length, input_list, list_weight):
     
     print(weighted_average)
     return weighted_average
+    
+# Or solved with numpy
+# import numpy as np
+# 
+# def calculate_weighted_mean(length, num_list, num_weight):
+# 
+#     weighted_average = round(np.average(num_list, weights=num_weight), 1)
+# 
+#     print(weighted_average)
+#     return weighted_average
 
 list_length = int(input())
 input_list = [int(num) for num in input().split(' ')]
